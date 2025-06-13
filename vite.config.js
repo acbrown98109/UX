ab7d90ded811639832008086515cf9a0ce8a9971
acbrown98109 <aaron.c.brown2@seattlecolleges.edu>
2025-06-12 22:50:39 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Update the base to match your repo name
 export default defineConfig({
+  base: '/UX/',
   plugins: [react()],
 })
